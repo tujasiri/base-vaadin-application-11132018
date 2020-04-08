41e9a20600dfa77864a07ff39c31ec8c89a6fd50
Tajiri Ujasiri <tujasiri@yahoo.com>
2020-04-07 22:59:45 -0500

    merged fixes to development

--- src/main/java/com/vaadin/root/model/RColor_.java
+++ src/main/java/com/vaadin/root/model/RColor_.java
@@ -5,11 +5,11 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2019-12-16T00:51:00.220-0600")
+@Generated(value="Dali", date="2020-04-07T18:34:35.889-0500")
 @StaticMetamodel(RColor.class)
 public class RColor_ {
 	public static volatile SingularAttribute<RColor, Integer> id;
 	public static volatile SingularAttribute<RColor, String> display;
-	public static volatile SingularAttribute<RColor, MerchTypes> merch_type;
 	public static volatile SingularAttribute<RColor, String> value;
+	public static volatile SingularAttribute<RColor, MerchTypes> merch_type;
 }
