c62ab2b5485c62161e236a6f61ecdb54d25390e0
Tajiri Ujasiri <tujasiri@yahoo.com>
2020-06-14 15:34:51 -0500

    E-mail order enchancements and pdf invoice creation

--- src/main/java/com/vaadin/root/utils/UIUtils.java
+++ src/main/java/com/vaadin/root/utils/UIUtils.java
@@ -1,9 +1,14 @@
 package com.vaadin.root.utils;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Base64;
+import java.util.List;
 //import java.mail
 import java.util.Properties;
 import java.util.UUID;
@@ -26,14 +31,20 @@ import javax.servlet.http.HttpServletRequest;
 
 import com.vaadin.icons.VaadinIcons;
 import com.vaadin.root.dao.DefaultDataService;
+import com.vaadin.root.dto.CartSingleton;
+import com.vaadin.root.dto.CheckoutCart;
 import com.vaadin.root.framework.StandardFooterLayout;
 import com.vaadin.root.framework.StandardHeaderLayout;
+import com.vaadin.root.framework.grids.ShoppingCartGrid;
 import com.vaadin.root.model.BusinessInfo;
+import com.vaadin.root.model.MerchTable;
 import com.vaadin.server.StreamResource;
 import com.vaadin.server.StreamResource.StreamSource;
 import com.vaadin.server.VaadinService;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Image;
+import com.itextpdf.html2pdf.HtmlConverter;
+import com.itextpdf.kernel.pdf.PdfWriter;
 
 public class UIUtils {
 	
@@ -71,14 +82,16 @@ public class UIUtils {
 	
 	public static void SendMessage() {
 		
+		ShoppingCartGrid grid = new ShoppingCartGrid();
+		
 		BusinessInfo businessInfo = DefaultDataService.getInstance().getBusinessInfoDao().findById(2L);
 		
 		Image headerBanner = UIUtils.byteArrayToImage(businessInfo.getBiHeader());
 		String encodedimg = Base64.getEncoder().encodeToString(businessInfo.getBiLogo());
 //		String cid = String.format("%s",UUID.randomUUID());
 //		String cid = "http://a.vimeocdn.com/si/email/Vimeo-logo-1a2e3b.png";
-//		String cid = "http://166.62.122.123:8080/website/VAADIN/images/banner.png";
-		String cid = "http://166.62.122.123:8080/website/images/banner.png";
+//		String cid = "http://166.62.122.123:8080/website/images/banner.png";
+		String cid = "http://166.62.122.123:8080/website/VAADIN/images/banner.png";
 
 		 // Recipient's email ID needs to be mentioned.
 //        String to = "truthuniversal@gmail.com";
@@ -125,25 +138,13 @@ public class UIUtils {
 //			MimeBodyPart messageBodyPart = new MimeBodyPart();
 
 			BodyPart headerBodyPart = new MimeBodyPart();
+			BodyPart pdfAttachmentPart = new MimeBodyPart();
 			BodyPart messageBodyPart = new MimeBodyPart();
-			
 
 			//Convert images to be added inline
 //			DataSource fds = new FileDataSource("/home/manisha/javamail-mini-logo.png");
 			DataSource imageds = new ByteArrayDataSource(businessInfo.getBiHeader(),"image/png");
 			
-		    String basePath = VaadinService.getCurrent().getBaseDirectory().getAbsolutePath();                
-		    
-		    System.out.println("basePath==>"+basePath);
-		    
-//		    HttpServletRequest req = null;
-//		    String servletPath = req.getServletContext().getContextPath();
-//		    System.out.println("servletPath==>"+servletPath);
-
-//File imagefile = new File();
-//	imagefile.createNewFile()
-			
-//			headerBodyPart.setDataHandler(new DataHandler(imageds));
 			
 			headerBodyPart.setHeader("Content-ID", "<"+cid+">");	
 			headerBodyPart.setDisposition(MimeBodyPart.INLINE);
@@ -159,7 +160,7 @@ public class UIUtils {
             message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
 
             // Set Subject: header field
-            message.setSubject("This is the Subject Line!");
+            message.setSubject("Truth Universal Music ==> Order #000000007");
 
             // Now set the actual message
 //            message.setText("<h>This is the actual message</h>");
@@ -167,24 +168,156 @@ public class UIUtils {
           
 			messageBodyPart = new MimeBodyPart();
 //            messageBodyPart.setContent("<img src=\"cid:"+cid+"\"><br><br><br><b>This is the actual message, with a banner above.</b>","text/html");
-            messageBodyPart.setContent("<img src=\""+cid+"\"><br><br><br><b>This is the actual message, with a banner above.</b>","text/html");
+			String msgHtml="<img src=\""+cid+"\"><br><br><br><b>This is the actual message, with a banner above.</b>";
+//			String msgHtml="";
+			msgHtml+=buildContentHtml();
+			
+			//create invoicePDF 
+			String base64EncodedPdf = UIUtils.createOrderPDF(msgHtml);
+			
+			System.out.println("msgHtml==>"+msgHtml);
+			
+            messageBodyPart.setContent(msgHtml.toString(),"text/hmtl;charset=utf-8");
 
 //			multipart.addBodyPart(headerBodyPart);
-			multipart.addBodyPart(messageBodyPart);
+//			multipart.addBodyPart(messageBodyPart);
 
+
+			DataSource pdfds = new ByteArrayDataSource(base64EncodedPdf ,"application/pdf");
+			pdfAttachmentPart.setDataHandler(new DataHandler(pdfds));
+			pdfAttachmentPart.setFileName("order_ordernumber.pdf");
+			pdfAttachmentPart.setDisposition(MimeBodyPart.ATTACHMENT);
+
+			multipart.addBodyPart(pdfAttachmentPart);
             message.setContent(multipart);
             
-            System.out.println("multipart ==>"+multipart.toString());
+            msgHtml+="<div>View/Download order invoice.<a href=\"http://166.62.122.123/invoices/truthuniversal_order.pdf\"></div>";            
+		
 
+            message.setText(msgHtml.toString(), "utf-8", "html");
+
+            System.out.println("OS==>"+UIUtils.getOS().toString());
+            System.out.println("Substring ==>"+UIUtils.getOS().toString().contains("Windows"));
             System.out.println("sending...");
             // Send message
             Transport.send(message);
             System.out.println("Sent message successfully....");
-        } catch (MessagingException mex) {
+        } catch (MessagingException | IOException mex) {
             mex.printStackTrace();
         }
 
 		
+	}
+	
+	private static	String buildContentHtml() {
+		String rowStyle="";
+		String htmlTable="<table style=\"border-collapse: collapse;width:100%;\"><tr>"
+				+ "<td><b>ITEM</b></td>"
+				+ "<td><b>DESCRIPTION</b></td>"
+				+ "<td><b>COST</b></td>"
+				+ "</tr>";
+//		String htmlTable="";
+//			htmlTable+= "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\">" +
+//					"<html>\n" + 
+
+		List<MerchTable> items = CartSingleton.getInstance().getCheckoutCart().itemsInCart();
+		
+		int idx = 1;
+		
+		for(MerchTable item:items){
+			
+			rowStyle=getRowStyle(idx++);
+			
+			htmlTable+= 
+				"<tr style=\""+rowStyle+"\">"+
+					"<td style=\"text-align: left;padding: 8px;\">"+item.getMtItemNum()+"</td>" +
+					"<td style=\"text-align: left;padding: 8px;\">"+item.getMtItemDescLong()+"</td>" +
+					"<td style=\"text-align: left;padding: 8px;\">"+item.getMtItemPrice()+"</td>" +
+					"</tr>";
+		}
+
+		rowStyle=getRowStyle(idx++);
+		//add tax row
+		htmlTable+= 
+		"<tr style=\""+rowStyle+"\">"+
+			"<td style=\"text-align: left;padding: 8px;\">TAX: </td>" +
+			"<td style=\"text-align: left;padding: 8px;\"></td>" +
+			"<td style=\"text-align: left;padding: 8px;\">"+"0.0"+
+			"</td>" +
+			"</tr>";
+
+		rowStyle=getRowStyle(idx++);
+		//add shipping row
+		htmlTable+= 
+		"<tr style=\""+rowStyle+"\">"+
+			"<td style=\"text-align: left;padding: 8px;\">SHIPPING: </td>" +
+			"<td style=\"text-align: left;padding: 8px;\"></td>" +
+			"<td style=\"text-align: left;padding: 8px;\">"+CartSingleton.getInstance().getCheckoutCart().calculateShipping()+
+			"</td>" +
+			"</tr>";
+		
+
+		rowStyle=getRowStyle(idx++);
+		//add total row
+		htmlTable+= 
+		"<tr style=\""+rowStyle+"\">"+
+			"<td style=\"text-align: left;padding: 8px;\">TOTAL: </td>" +
+			"<td style=\"text-align: left;padding: 8px;\"></td>" +
+			"<td style=\"text-align: left;padding: 8px;\"></td>" +
+			"<td style=\"text-align: left;padding: 8px;\">"+CartSingleton.getInstance().getCheckoutCart().calculateTotal()+
+			"</td>" +
+			"</tr>";
+		
+
+		htmlTable+="</table>";
+
+
+		return htmlTable;
+	}
+	
+	private static String getRowStyle(int rowIndex){
+		return rowIndex % 2 == 0 ? "background-color: #f2f2f2;" : "background-color: #ffffff;";
+		
+	}
+	
+	public static String createOrderPDF(String htmlString) {
+
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		String encodedPdf = "";
+
+    	try {
+			HtmlConverter.convertToPdf(htmlString, new FileOutputStream(UIUtils.getDocumentRoot()+
+					 "\\truthuniversal-order.pdf"));
+			HtmlConverter.convertToPdf(htmlString, baos);
+			encodedPdf = Base64.getEncoder().encodeToString(baos.toByteArray());
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return encodedPdf;
+	}
+	
+	private static String getOS(){
+		return System.getProperty("os.name");
+	}
+	
+	private static String getDocumentRoot() {
+		String docRoot="";
+		
+		String serverOS = UIUtils.getOS();
+		
+		if(serverOS.contains("Windows"))
+			docRoot="C:\\Users\\tujasiri2\\AppData\\Local\\Packages\\"
+					+ "CanonicalGroupLimited.UbuntuonWindows_79rhkp1fndgsc\\"
+					+ "LocalState\\rootfs\\var\\www\\html\\invoices";
+
+		if(serverOS.contains("Linux"))
+			docRoot="/var/www/html/invoices";
+
+		return docRoot;
 	}
 	
 }
