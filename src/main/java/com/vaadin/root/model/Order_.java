b793fa348a10db3e13e4b4f9688394af6e18fdc0
tujasiri2 <tujasiri2@US5CD9011ZXL.CSCMWS.CSCMWS.COM>
2022-12-19 11:52:50 -0600

    local changes

--- src/main/java/com/vaadin/root/model/Order_.java
+++ src/main/java/com/vaadin/root/model/Order_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2020-07-07T21:14:34.520-0500")
+@Generated(value="Dali", date="2021-06-10T02:00:43.381-0500")
 @StaticMetamodel(Order.class)
 public class Order_ {
 	public static volatile SingularAttribute<Order, Long> orSeq;
